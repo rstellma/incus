308872cc3120c4964cb2e45fe813f069289c6634
ralf <rst_77@gmx.net>
2026-06-30 22:41:18 +0200

    Incus configuration backup

--- backup.sh
+++ backup.sh
@@ -5,16 +5,15 @@ set -eu
 set -o pipefail
 
 declare -gr __HERE=$(cd $(dirname $0) && pwd)
-declare -gr __FLD_BCKP=${__HERE}/backup
 
 for p in $(incus profile ls -cn -fcsv | grep -v ^default); do
-	incus profile show $p | sed -e 's|^- /1.0/instances.*||g;/^$/d' > ${__FLD_BCKP}/profiles/${p}.yml
+	incus profile show $p | sed -e 's|^- /1.0/instances.*||g;/^$/d' > ${__HERE}/profiles/${p}.yml
 done
 
 for n in $(incus network ls -cn -fcsv | sed 's/^lo//g;s/enp3s0//g;/^$/d'); do
-	incus network show $n | sed 's|^- /1.0/instances.*||g;/^$/d' > ${__FLD_BCKP}/networks/${n}.yml
+	incus network show $n | sed 's|^- /1.0/instances.*||g;/^$/d' > ${__HERE}/networks/${n}.yml
 done
 
 for s in $(incus storage ls -cn -fcsv); do
-	incus storage show $s | sed -e 's|^- /1.0/instances.*||g;/^$/d' > ${__FLD_BCKP}/storage/${s}.yml
+	incus storage show $s | sed -e 's|^- /1.0/instances.*||g;/^$/d' > ${__HERE}/storage/${s}.yml
 done
