8916b5e33ccf77673ae349a2640d6a726d1b51df
ralf <rst_77@gmx.net>
2026-07-01 08:09:37 +0200

    back up instances

--- backup.sh
+++ backup.sh
@@ -6,14 +6,20 @@ set -o pipefail
 
 declare -gr __HERE=$(cd $(dirname $0) && pwd)
 
+
+
 for p in $(incus profile ls -cn -fcsv | grep -v ^default); do
-	incus profile show $p | sed -e 's|^- /1.0/instances.*||g;/^$/d' > ${__HERE}/profiles/${p}.yml
+	incus profile show $p > ${__HERE}/profiles/${p}.yml
 done
 
-for n in $(incus network ls -cn -fcsv | sed 's/^lo//g;s/enp3s0//g;/^$/d'); do
-	incus network show $n | sed 's|^- /1.0/instances.*||g;/^$/d' > ${__HERE}/networks/${n}.yml
+for n in $(incus network ls -cn -fcsv | sed '/^lo/d;/enp3s0/d'); do
+	incus network show $n > ${__HERE}/networks/${n}.yml
 done
 
 for s in $(incus storage ls -cn -fcsv); do
-	incus storage show $s | sed -e 's|^- /1.0/instances.*||g;/^$/d' > ${__HERE}/storage/${s}.yml
+	incus storage show $s > ${__HERE}/storage/${s}.yml
+done
+
+for i in $(incus ls -cn -fcsv); do
+	incus export $i ${__HERE}/instances/${i}.tar.gz
 done
