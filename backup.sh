610442ad8eae3d12284b0cc51034b925e9b99ceb
ralf <rst_77@gmx.net>
2026-07-01 18:19:56 +0200

    shellcheck

--- backup.sh
+++ backup.sh
@@ -4,23 +4,23 @@
 set -eu
 set -o pipefail
 
-declare -gr __HERE=$(cd $(dirname $0) && pwd)
+declare -gr __HERE=$(cd "$(dirname "$0")" && pwd)
 
 
 
 for p in $(incus profile ls -cn -fcsv | grep -v ^default); do
-	incus profile show $p > ${__HERE}/profiles/${p}.yml
+	incus profile show "$p" > "${__HERE}"/profiles/"${p}".yml
 done
 
 for n in $(incus network ls -cn -fcsv | sed '/^lo/d;/enp3s0/d'); do
-	incus network show $n > ${__HERE}/networks/${n}.yml
+	incus network show "$n" > "${__HERE}"/networks/"${n}".yml
 done
 
 for s in $(incus storage ls -cn -fcsv); do
-	incus storage show $s > ${__HERE}/storage/${s}.yml
+	incus storage show "$s" > "${__HERE}"/storage/"${s}".yml
 done
 
 for i in $(incus ls -cn -fcsv); do
-	incus config show $i > ${__HERE}/instances/${i}.yml
-	incus export $i ${__HERE}/instances/${i}.tar.gz
+	incus config show "$i" > "${__HERE}"/instances/"${i}".yml
+	incus export "$i" "${__HERE}"/instances/"${i}".tar.gz
 done
--- restore.sh
+++ restore.sh
@@ -4,28 +4,28 @@
 set -eu
 set -o pipefail
 
-declare -gr __HERE=$(cd $(dirname $0) && pwd)
+declare -gr __HERE=$(cd "$(dirname "$0")" && pwd)
 
 
 
-for p in $(ls ${__HERE}/profiles/*.yml); do
-	incus profile create $(basename ${p%.*}) < $p
+for p in "${__HERE}"/profiles/*.yml; do
+	incus profile create "$(basename "${p%.*}")" < "$p"
 done
 
-for n in $(ls ${__HERE}/networks/*.yml); do
-	incus network create $(basename ${n%.*}) < $n
+for n in "${__HERE}"/networks/*.yml; do
+	incus network create "$(basename "${n%.*}")" < "$n"
 done
 
-for s in $(ls ${__HERE}/storage/*.yml); do
-	incus storage create $(basename ${s%.*}) < $s
+for s in "${__HERE}"/storage/*.yml; do
+	incus storage create "$(basename "${s%.*}")" < "$s"
 done
 
-for i in $(ls ${__HERE}/instances/*.tar.gz); do
-	incus import $i $(basename ${i%%.*})
+for i in "${__HERE}"/instances/*.tar.gz; do
+	incus import "$i" "$(basename "${i%%.*}")"
 	# This step is only successful if an instance with
 	# the same network interface does not already exist.
 	# Otherwise, it fails with:
 	# > "Error: Failed start validation for device "eth0":
 	# > MAC address "foobar" already defined on another NIC"
-	incus start $(basename ${i%%.*})
+	incus start "$(basename "${i%%.*}")"
 done
