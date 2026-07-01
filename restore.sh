06af9bbfc656d76ae1bcda341c2b67fc1908671f
ralf <rst_77@gmx.net>
2026-07-01 08:14:26 +0200

    restore instances

--- restore.sh
+++ restore.sh
@@ -6,6 +6,8 @@ set -o pipefail
 
 declare -gr __HERE=$(cd $(dirname $0) && pwd)
 
+
+
 for p in $(ls ${__HERE}/profiles/*.yml); do
 	incus profile create $(basename ${p%.*}) < $p
 done
@@ -16,4 +18,14 @@ done
 
 for s in $(ls ${__HERE}/storage/*.yml); do
 	incus storage create $(basename ${s%.*}) < $s
+done
+
+for i in $(ls ${__HERE}/instances/*.tar.gz); do
+	incus import $i $(basename ${i%%.*})
+	# This step is only successful if an instance with
+	# the same network interface does not already exist.
+	# Otherwise, it fails with:
+	# > "Error: Failed start validation for device "eth0":
+	# > MAC address "foobar" already defined on another NIC"
+	incus start $(basename ${i%%.*})
 done
